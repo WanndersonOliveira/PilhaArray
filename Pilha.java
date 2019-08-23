26bf0adbe1d6e3669300d32bacb9e43971095d84
Wannderson Oliveira <wannders.35@hotmail.com>
2019-08-23 10:33:32 -0300

    Update Pilha.java

--- Pilha.java
+++ Pilha.java
@@ -1,12 +1,7 @@
 public interface Pilha<Objeto>{
-	public int sizePreta();
-	public int sizeVermelha();
-	public boolean isEmptyPreta();
-	public boolean isEmptyVermelha();
-	public Objeto topPreta() throws EPilhaVazia;
-	public Objeto topVermelha() throws EPilhaVazia;
-	public void pushPreta(Objeto o);
-	public void pushVermelha(Objeto o);
-	public Objeto popPreta() throws EPilhaVazia;
-	public Objeto popVermelha() throws EPilhaVazia;
+	public int size();
+	public boolean isEmpty();
+	public Objeto top() throws EPilhaVazia;
+	public void push(Objeto o);
+	public Objeto pop() throws EPilhaVazia;
 }
